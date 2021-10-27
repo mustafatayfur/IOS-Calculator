8d5a6c6d34bb5fee090644e53f42b41e2ea50d23
mustafa <mltm0204@gmail.com>
2021-10-27 16:30:53 +0300

    ...

---
+++ index.js
@@ -0,0 +1,27 @@
+const numberButton =  document.querySelector(".button number");
+const clearButton = document.querySelector(".clear grey");
+const plusMinus = document.querySelector(".grey");
+const deleteButton = document.querySelector(".icon grey");
+const operators = document.querySelector(".operator");
+const divideOperand = documen.querySelector(".divide");
+const multipleOperand = document.querySelector(".multiple");
+const minusOperand = document.querySelector(".minus");
+const plusOperand = document.querySelector(".plus");
+const equalOperand = document.querySelector(".equal");
+const commaButton = document.querySelector(".comma");
+const output1 = document.querySelector(".result1");
+const output2 = document.querySelector(".result2");
+let display1 = document.querySelector(".result1");
+let display2 = document.querySelector("result2");
+
+
+
+
+
+numberButton.map( button => {
+    button.addEventlistener('click',(e) => {
+        switch(e.target.innerText){
+            case ''
+        }
+    });
+});
